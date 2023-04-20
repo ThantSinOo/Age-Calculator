fd54283500e170ab08b5ebbc384f2a50b786d544
Reggae Homer <mgthantsinmm21@gmail.com>
2023-04-20 23:45:59 +0630

    add js function for calculation

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("helloe");
+const day_input = document.querySelectorAll('#days_inputbox');
+const month_input = document.querySelectorAll('#months_inputbox');
+const years_input = document.querySelectorAll('#years_inputbox');
+const day_result = document.querySelectorAll('#days_result');
+const month_result = document.querySelectorAll('#months_result');
+const years_result = document.querySelectorAll('#years_result');
+
